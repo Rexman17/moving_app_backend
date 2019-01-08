b24bf483f859fe970f19b99a8866925dc98a11b0
Rexman17 <a.stone1794@gmail.com>
2019-01-08 16:40:04 -0500

    created User model, controller, and table

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,53 @@
+class Api::V1::UsersController < ApplicationController
+
+  before_action :find_user, only: [:show]
+
+ def index
+   @users = User.all
+   render json: @users
+ end
+
+ # def profile
+ #    render json: { user: UserSerializer.new(current_user) }, status: :accepted
+ #  end
+
+ def show
+    render json: @user, status: :ok
+  end
+
+  # def create
+  #   @user = User.create(user_params)
+  #   if @user.valid?
+  #     @token = encode_token(user_id: @user.id)
+  #     render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created
+  #   else
+  #     render json: { error: 'failed to create user' }, status: :not_acceptable
+  #   end
+  # end
+
+  # def destroy
+  #   @user.destroy
+  # end
+
+ def update
+   @user.update(user_params)
+   if @user.save
+     render json: @user, status: :accepted
+   else
+     render json: { errors: @user.errors.full_messages }, status: :unprocessible_entity
+   end
+ end
+
+ private
+
+ # # TODO: when i add auth, will have to add username and password
+ # password_digest to the schema though
+ def user_params
+   params.permit(:name)
+ end
+
+ def find_user
+   @user = User.find(params[:id])
+ end
+
+end
